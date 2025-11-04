e176b684deffb34402611fa163ebe11f171c4cc1
weng <weng2019157365@gmail.com>
2025-11-03 22:46:16 -0500

    Created prototype for stop and trip class

--- backend/models/stop.py
+++ backend/models/stop.py
@@ -1,1 +1,60 @@
 # Stop model - defines stop data structure and methods
+
+from enum import Enum
+
+class StopType(Enum):
+    FOOD = "images/planets.png" #Going to be filepaths for images later
+    REST = "images/planets.png" #relative from frontend folder
+    FUEL = "images/planets.png"
+    ENTERTAINMENT = "images/planets.png"
+    MISC = "images/planets.png"
+
+class Stop:
+    def __init__(self, location, type, time=0, cost=0):
+        self.location = location #lat/lng tuple
+        self.type = type  # Should be of type StopType
+        self.time = time  # in minutes
+        self.cost = cost  # in dollars
+
+    def get_location(self):
+        return self.location
+    
+    def get_type(self):
+        return self.type
+
+    def get_time(self):
+        return self.time
+    
+    def get_cost(self):
+        return self.cost
+    
+    def get_image(self):
+        return self.type.value
+    
+    def set_location(location):
+        self.location = location
+
+    def set_type(type):
+        self.type = type
+
+    def set_time(time):
+        self.time = time
+        
+    def set_cost(cost):
+        self.cost = cost
+
+    def to_dict(self):
+        return {
+            'location' : self.location,
+            'image' : self.type.value,
+            'time' : self.time,
+            'cost' : self.cost
+        }
+    
+    def save_to_db(self):
+        # Placeholder
+        pass
+
+    def load_from_db(self):
+        # Placeholder
+        pass
--- backend/models/trip.py
+++ backend/models/trip.py
@@ -1,1 +1,42 @@
 # Trip model - defines trip data structure and methods
+from stop import Stop
+
+class Trip:
+    def __init__(self):
+        self.stops = []
+
+    def add_stop(self, stop):
+        self.stops.append(stop)
+
+    def delete_stop(self, index):
+        if 0 <= index < len(self.stops):
+            del self.stops[index]
+
+    def get_stops():
+        return stops
+    
+    def get_stop(index):
+        pass
+
+    def total_stops():
+        return len(stops)
+    
+    def total_time():
+        pass
+
+    def total_cost():
+        pass
+    
+    def sum_time():
+        pass
+
+    def to_dict(self):
+        return [stop.to_dict() for stop in self.stops]
+
+    def save_to_db(self):
+        # Placeholder
+        pass
+
+    def load_from_db(self):
+        # Placeholder
+        pass
